71fac2a2185d6d0092bad6d5f94b1949b03f082c
2Jinx <113981832+2Jinx@users.noreply.github.com>
2023-12-23 21:15:09 +0300

    Практика №3

--- homework_3/MyHttpServer/MyHttpServer/Program.cs
+++ homework_3/MyHttpServer/MyHttpServer/Program.cs
@@ -11,7 +11,7 @@ class Program
         if (File.Exists("appsettings.json"))
         {
             string json = File.ReadAllText("appsettings.json");
-            bool isAlive = true;
+
             // Десериализация JSON в словарь
             Dictionary<string, string> data = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
             // Извлечение порта и адреса из словаря
@@ -24,45 +24,27 @@ class Program
                 server.Start(); // начинаем прослушивать входящие подключения
                 Console.WriteLine("Сервер начал работу!");
 
-                CancellationTokenSource cts = new CancellationTokenSource();
-                CancellationToken token = cts.Token;
-
-                while (isAlive)
+                // отправляемый в ответ код html возвращает
+                string path = "static/index.html";
+                string responseText = "";
+                using (StreamReader reader = new StreamReader(path))
                 {
-                    var inputThread = new Thread(() =>
-                    {
-                        while (isAlive)
-                        {
-                            if (Console.ReadLine().ToLower() == "stop")
-                            {
-                                isAlive = false;
-                                cts.Cancel();
-                            }
-                        }
-                    });
-                    inputThread.Start();
-                    // получаем контекст
-                    var context = await server.GetContextAsync();
-
-                    var response = context.Response;
-                    // отправляемый в ответ код html возвращает
-                    string path = "static/index.html";
-                    string responseText = "";
-                    using (StreamReader reader = new StreamReader(path))
-                    {
-                        string html = await reader.ReadToEndAsync();
-                        responseText = html;
-                    }
-                    byte[] buffer = Encoding.UTF8.GetBytes(responseText);
-                    // получаем поток ответа и пишем в него ответ
-                    response.ContentLength64 = buffer.Length;
-                    using Stream output = response.OutputStream;
-                    // отправляем данные
-                    await output.WriteAsync(buffer);
-                    await output.FlushAsync();
-
-                    Console.WriteLine("Запрос обработан");
+                    string html = await reader.ReadToEndAsync();
+                    responseText = html;
                 }
+                // получаем контекст
+                var context = await server.GetContextAsync();
+
+                var response = context.Response;
+
+                byte[] buffer = Encoding.UTF8.GetBytes(responseText);
+                // получаем поток ответа и пишем в него ответ
+                response.ContentLength64 = buffer.Length;
+                using Stream output = response.OutputStream;
+                // отправляем данные
+                await output.WriteAsync(buffer);
+                await output.FlushAsync();
+                Console.WriteLine("Запрос обработан");
 
                 server.Stop();
                 Console.WriteLine("Сервер закончил работу!");
